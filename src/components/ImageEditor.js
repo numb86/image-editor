3e483e0f5d6eb45f3c0a224382683923ed928f07
numb86 <86numb@gmail.com>
2017-09-13 23:07:26 +0900

    image/png と image/jpeg のみ、アップロードを許可するようにした。

--- src/components/ImageEditor.js
+++ src/components/ImageEditor.js
@@ -4,6 +4,7 @@ import FileTransferButtons from './FileTransferButtons';
 
 const RESIZE_RATIO = 0.5;
 const DOWNLOAD_IMAGE_FILE_TYPE = 'png';
+const ALLOW_FILE_TYPES = ['image/png', 'image/jpeg'];
 
 const trimFileNameExtension = fileName => {
   const periodPosition = fileName.lastIndexOf('.');
@@ -15,6 +16,11 @@ const autoDownload = (url, fileName) => {
   elem.href = url;
   elem.download = fileName;
   elem.click();
+};
+
+const isAllowedFileType = (uploadedFileType, allowList) => {
+  const result = allowList.filter(allowFile => allowFile === uploadedFileType);
+  return !!(result.length > 0) || false;
 };
 
 const resizeImage = (imageDataUrl, ratio) =>
@@ -39,6 +45,7 @@ export default class ImageEditor extends React.Component {
     this.state = {
       previewImageDataUrl: null,
       downloadImageFileName: null,
+      errorMessage: null,
     };
     this.onImageSelected = this.onImageSelected.bind(this);
     this.onImageLoad = this.onImageLoad.bind(this);
@@ -50,6 +57,7 @@ export default class ImageEditor extends React.Component {
         previewImageDataUrl: res,
         /* prettier-ignore */
         downloadImageFileName: `${trimFileNameExtension(originalFileName)}.${DOWNLOAD_IMAGE_FILE_TYPE}`,
+        errorMessage: null,
       });
       autoDownload(
         this.state.previewImageDataUrl,
@@ -60,6 +68,10 @@ export default class ImageEditor extends React.Component {
 
   onImageSelected(e) {
     const file = e.target.files[0];
+    if (!isAllowedFileType(file.type, ALLOW_FILE_TYPES)) {
+      this.setState({errorMessage: '対応しているファイル形式はjpegとpngのみです。'});
+      return;
+    }
     const fileReader = new FileReader();
     fileReader.onload = () => {
       this.onImageLoad(fileReader.result, file.name);
@@ -68,7 +80,11 @@ export default class ImageEditor extends React.Component {
   }
 
   render() {
-    const {previewImageDataUrl, downloadImageFileName} = this.state;
+    const {
+      previewImageDataUrl,
+      downloadImageFileName,
+      errorMessage,
+    } = this.state;
     return (
       <div>
         <FileTransferButtons
@@ -76,6 +92,7 @@ export default class ImageEditor extends React.Component {
           previewImageDataUrl={previewImageDataUrl}
           downloadImageFileName={downloadImageFileName}
         />
+        {errorMessage && <p>{errorMessage}</p>}
         <p>
           <img src={previewImageDataUrl} alt="ここに画像が表示されます。" />
         </p>
