b5598edaa63c621f69d7d324ce555516af7cc9fb
numb86 <86numb@gmail.com>
2017-10-31 06:26:10 +0900

    onChange に渡す値を e ではなく e.targe.value にした。

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function TextForm(props: {
   text: string,
   onSubmit: (e: Event) => void,
-  onChange: (e: Event) => void,
+  onChange: (text: string) => void,
 }) {
   return (
     <form onSubmit={e => props.onSubmit(e)}>
@@ -12,7 +12,7 @@ export default function TextForm(props: {
         type="text"
         placeholder="テキストを入力"
         value={props.text}
-        onChange={e => props.onChange(e)}
+        onChange={e => props.onChange(e.target.value)}
         onKeyPress={e => {
           if (e.charCode === 13) props.onSubmit(e);
         }}
