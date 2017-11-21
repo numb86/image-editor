6dae4cad65038e709686cb67a32f6167ae1c088c
numb86 <86numb@gmail.com>
2017-11-22 06:43:42 +0900

    テストを sinon.spy を使ったものに書き換えた。

--- src/components/__tests__/TextForm.test.js
+++ src/components/__tests__/TextForm.test.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {spy} from 'sinon';
 import assert from 'assert';
 
 import TextForm from '../TextForm';
 
 describe('TextForm', () => {
-  let submitCount = 0;
-  let text = '';
-  const onSubmit = () => {
-    submitCount += 1;
-  };
-  const onChange = value => {
-    text = value;
-  };
-  const wrapper = shallow(
-    <TextForm text={'foo'} onSubmit={onSubmit} onChange={onChange} />
-  );
-  const textBox = wrapper.find('input').first();
+  let onSubmit;
+  let onChange;
+  let wrapper;
+  let textBox;
   beforeEach(() => {
-    submitCount = 0;
-    text = '';
+    onSubmit = spy();
+    onChange = spy();
+    wrapper = shallow(
+      <TextForm text={'foo'} onSubmit={onSubmit} onChange={onChange} />
+    );
+    textBox = wrapper.find('input').first();
   });
 
   it('props.textの値がテキストボックスの値になる', () => {
@@ -28,19 +25,18 @@ describe('TextForm', () => {
   });
 
   it('changeイベントによってprops.onChangeが実行される', () => {
-    const newValue = 'hogehoge';
-    textBox.simulate('change', {target: {value: newValue}});
-    assert(text === newValue);
+    textBox.simulate('change', {target: {value: 'hogehoge'}});
+    assert(onChange.callCount === 1);
   });
 
   it('formの送信やEnterの押下でprops.onSubmitが実行される', () => {
     assert(wrapper.find('input').get(1).props.type === 'submit');
-    assert(submitCount === 0);
+    assert(onSubmit.callCount === 0);
     wrapper.find('form').simulate('submit', {preventDefault() {}});
-    assert(submitCount === 1);
+    assert(onSubmit.callCount === 1);
     textBox.simulate('keyPress', {charCode: 16}); // shiftを押下
-    assert(submitCount === 1);
+    assert(onSubmit.callCount === 1);
     textBox.simulate('keyPress', {charCode: 13}); // enterを押下
-    assert(submitCount === 2);
+    assert(onSubmit.callCount === 2);
   });
 });
