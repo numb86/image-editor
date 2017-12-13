b4fbf607639b8a6f7e5fc6fdbb04b394fbc26a87
numb86 <86numb@gmail.com>
2017-12-13 16:45:24 +0900

    ボタンをレンダリングする処理を共通化した。

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,12 @@ type State = {
   selectMenu: SelectMenu,
 };
 
+const TEXT_BUTTON_LIST = [
+  {value: 'sketch', label: 'スケッチ'},
+  {value: 'resizeAndRotate', label: 'リサイズと回転'},
+  {value: 'colorTone', label: '色調変更'},
+];
+
 export default class Header extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
@@ -31,40 +37,20 @@ export default class Header extends React.Component<Props, State> {
           <button className="fa fa-repeat header-menu-button header-menu-button__icon" />
           <button className="fa fa-upload header-menu-button header-menu-button__icon" />
           <button className="fa fa-download header-menu-button header-menu-button__icon" />
-          <button
-            className={ClassNames(
-              'header-menu-button header-menu-button__text',
-              {
-                'header-menu-button__text-active': selectMenu === 'sketch',
-              }
-            )}
-            onClick={() => this.onClickTextButton('sketch')}
-          >
-            スケッチ
-          </button>
-          <button
-            className={ClassNames(
-              'header-menu-button header-menu-button__text',
-              {
-                'header-menu-button__text-active':
-                  selectMenu === 'resizeAndRotate',
-              }
-            )}
-            onClick={() => this.onClickTextButton('resizeAndRotate')}
-          >
-            リサイズと回転
-          </button>
-          <button
-            className={ClassNames(
-              'header-menu-button header-menu-button__text',
-              {
-                'header-menu-button__text-active': selectMenu === 'colorTone',
-              }
-            )}
-            onClick={() => this.onClickTextButton('colorTone')}
-          >
-            色調変更
-          </button>
+          {TEXT_BUTTON_LIST.map(text => (
+            <button
+              key={text.value}
+              className={ClassNames(
+                'header-menu-button header-menu-button__text',
+                {
+                  'header-menu-button__text-active': selectMenu === text.value,
+                }
+              )}
+              onClick={() => this.onClickTextButton(text.value)}
+            >
+              {text.label}
+            </button>
+          ))}
         </div>
         <HeaderSubMenu selectMenu={selectMenu} />
       </div>
