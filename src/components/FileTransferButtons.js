2daf93ca15f4e2545abb002a2679862ce2a50c5b
numb86 <86numb@gmail.com>
2017-12-14 15:26:21 +0900

    使わなくなったアップロードボタンを削除。

--- src/components/FileTransferButtons.js
+++ src/components/FileTransferButtons.js
@@ -1,17 +1,14 @@
 // @flow
 import React from 'react';
 
-import UploadButton from './UploadButton';
 import DownloadButton from './DownloadButton';
 
 export default function FileTransferButtons(props: {
-  onImageSelected: (files: FileList) => void,
   previewImageDataUrl: string | null,
   downloadImageFileName: string | null,
 }) {
   return (
     <div className="file-transfer-button-area">
-      <UploadButton onChange={props.onImageSelected} />
       <DownloadButton
         href={props.previewImageDataUrl}
         download={props.downloadImageFileName}
