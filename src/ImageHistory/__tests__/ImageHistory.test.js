d2f07926808ed889fad1e1919e7a9631aec7b230
numb86 <86numb@gmail.com>
2017-12-24 09:47:00 +0900

    historyには文字列ではなくオブジェクトを入れるようにした。

--- src/ImageHistory/__tests__/ImageHistory.test.js
+++ src/ImageHistory/__tests__/ImageHistory.test.js
@@ -3,9 +3,9 @@ import assert from 'assert';
 import ImageHistory from '../ImageHistory';
 
 describe('ImageHistory', () => {
-  const stringAbc = 'abc';
-  const stringQwerty = 'qwerty';
-  const stringFoo = 'foo';
+  const abc = {originalData: 'abc', editedData: 'editedAbc'};
+  const qwerty = {originalData: 'qwerty', editedData: 'editedQwerty'};
+  const foo = {originalData: 'foo', editedData: 'editedQwerty'};
   let imageHistory;
   beforeEach(() => {
     imageHistory = new ImageHistory();
@@ -21,36 +21,36 @@ describe('ImageHistory', () => {
   });
   describe('update', () => {
     it('渡された引数が、historyの先頭の要素になる', () => {
-      imageHistory.update(stringAbc);
-      assert(imageHistory.history[0] === stringAbc);
-      imageHistory.update(stringQwerty);
-      assert(imageHistory.history[0] === stringQwerty);
+      imageHistory.update(abc);
+      assert(imageHistory.history[0].originalData === abc.originalData);
+      imageHistory.update(qwerty);
+      assert(imageHistory.history[0].originalData === qwerty.originalData);
     });
     it('positionが0になる', () => {
       imageHistory.position = 3;
       assert(imageHistory.position !== 0);
-      imageHistory.update(stringAbc);
+      imageHistory.update(abc);
       assert(imageHistory.position === 0);
     });
     it('position > 0 の場合、positionより前の要素は全て消去し前に詰める', () => {
-      imageHistory.update(stringAbc);
-      imageHistory.update(stringQwerty);
-      imageHistory.update(stringFoo);
+      imageHistory.update(abc);
+      imageHistory.update(qwerty);
+      imageHistory.update(foo);
       imageHistory.position = 2;
-      imageHistory.update(stringAbc);
+      imageHistory.update(abc);
       assert(imageHistory.history.length === 2);
-      assert(imageHistory.history[0] === stringAbc);
-      assert(imageHistory.history[1] === stringAbc);
+      assert(imageHistory.history[0].originalData === abc.originalData);
+      assert(imageHistory.history[1].originalData === abc.originalData);
     });
   });
   describe('get', () => {
     it('history[position]の要素を取得する', () => {
-      imageHistory.update(stringAbc);
-      imageHistory.update(stringQwerty);
+      imageHistory.update(abc);
+      imageHistory.update(qwerty);
       imageHistory.position = 1;
-      assert(imageHistory.get() === stringAbc);
+      assert(imageHistory.get().originalData === abc.originalData);
       imageHistory.position = 0;
-      assert(imageHistory.get() === stringQwerty);
+      assert(imageHistory.get().originalData === qwerty.originalData);
     });
     it('historyが空ならnullを返す', () => {
       assert(imageHistory.history.length === 0);
@@ -59,19 +59,19 @@ describe('ImageHistory', () => {
   });
   describe('back', () => {
     it('positionの数値を一つ増やす', () => {
-      imageHistory.update(stringAbc);
-      imageHistory.update(stringQwerty);
-      imageHistory.update(stringFoo);
+      imageHistory.update(abc);
+      imageHistory.update(qwerty);
+      imageHistory.update(foo);
       imageHistory.back();
       assert(imageHistory.position === 1);
       imageHistory.back();
       assert(imageHistory.position === 2);
     });
     it('(history.length - 1)よりは増えない', () => {
-      imageHistory.update(stringAbc);
+      imageHistory.update(abc);
       imageHistory.back();
       assert(imageHistory.position === 0);
-      imageHistory.update(stringQwerty);
+      imageHistory.update(qwerty);
       imageHistory.back();
       imageHistory.back();
       assert(imageHistory.position === 1);
@@ -85,9 +85,9 @@ describe('ImageHistory', () => {
   });
   describe('forward', () => {
     it('positionの数値を一つ減らす', () => {
-      imageHistory.update(stringAbc);
-      imageHistory.update(stringQwerty);
-      imageHistory.update(stringFoo);
+      imageHistory.update(abc);
+      imageHistory.update(qwerty);
+      imageHistory.update(foo);
       imageHistory.position = 2;
       imageHistory.forward();
       assert(imageHistory.position === 1);
@@ -95,14 +95,13 @@ describe('ImageHistory', () => {
       assert(imageHistory.position === 0);
     });
     it('0よりは減らない', () => {
-      imageHistory.update(stringAbc);
+      imageHistory.update(abc);
       imageHistory.position = 1;
       imageHistory.forward();
       assert(imageHistory.position === 0);
       imageHistory.forward();
       assert(imageHistory.position === 0);
     });
-    it('', () => {});
     it('historyが空なら何も起こらない', () => {
       assert(imageHistory.history.length === 0);
       assert(imageHistory.position === null);
