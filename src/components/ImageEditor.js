810a57d686e70a1aab64893681593154e7bcb7eb
numb86 <86numb@gmail.com>
2017-12-29 20:48:55 +0900

    SketchCanvas は常に表示させるように変えた。
    画像が操作される度に再描画され初期化されてしまう実装だったため。

--- src/components/ImageEditor.js
+++ src/components/ImageEditor.js
@@ -187,7 +187,7 @@ export default class ImageEditor extends React.Component<Props, State> {
               onDrop={this.onImageSelected}
             />
           )}
-          {previewImageDataUrl && <SketchCanvas />}
+          <SketchCanvas />
         </div>
         <Header
           onImageSelected={this.onImageSelected}
