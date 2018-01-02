4db16d2ac2cbb24816dac4c85772a7b1825e84a4
numb86 <86numb@gmail.com>
2018-01-02 16:30:04 +0900

    画像を合成するためのプログラムをまとめた synthesizeImages を作成。

---
+++ src/synthesizeImages.js
@@ -0,0 +1,34 @@
+function loadImage(dataUrl) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.src = dataUrl;
+  });
+}
+
+function generateBaseCanvas(baseImage) {
+  return loadImage(baseImage).then(image => {
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+    return {canvas, ctx};
+  });
+}
+
+export default function synthesizeImages(dataUrlList) {
+  return generateBaseCanvas(dataUrlList[0])
+    .then(({canvas, ctx}) => {
+      dataUrlList.shift();
+      return Promise.all(
+        dataUrlList.map(data => loadImage(data))
+      ).then(images => ({canvas, ctx, images}));
+    })
+    .then(({canvas, ctx, images}) => {
+      images.forEach(image => {
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+      });
+      return canvas.toDataURL();
+    });
+}
