968e708f13bd55841521bd1b00b91f007abe9bdc
numb86 <86numb@gmail.com>
2018-01-27 01:29:04 +0900

    propsで表示倍率を制御できるようにした。

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-export default function Display({width, height, children}) {
+export default function Display({
+  width,
+  height,
+  magnificationPercent,
+  children,
+}) {
   return (
     <div
       className="display"
-      style={{width: `${width}px`, height: `${height}px`}}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        transform: `scale(${magnificationPercent / 100})`,
+      }}
     >
       {children}
     </div>
