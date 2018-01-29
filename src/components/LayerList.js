d1210b83a71c387d422ff09a48ec434315a38710
numb86 <86numb@gmail.com>
2018-01-29 13:15:55 +0900

    LayerList が任意の ViewLayer にアクセスできるようにした。

--- src/components/LayerList.js
+++ src/components/LayerList.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 import ViewLayer from './ViewLayer';
 
-export default function LayerList({viewLayerDataList}) {
-  return viewLayerDataList.map(data => {
-    const {id} = data;
-    delete data.id;
-    return <ViewLayer key={id} {...data} />;
-  });
+export default class LayerList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ids = [];
+  }
+  componentDidMount() {
+    console.log(this.ids);
+  }
+  render() {
+    return this.props.viewLayerDataList.map(data => {
+      const {id} = data;
+      delete data.id;
+      return (
+        <ViewLayer
+          ref={ref => this.ids.push({id, ViewLayer: ref})}
+          key={id}
+          {...data}
+        />
+      );
+    });
+  }
 }
