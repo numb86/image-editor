a9f68c98e9e9ce7bbe00c78e8f049106567456f2
numb86 <86numb@gmail.com>
2018-01-30 11:35:49 +0900

    Revert "LayerList が任意の ViewLayer にアクセスできるようにした。"
    This reverts commit d1210b83a71c387d422ff09a48ec434315a38710.

--- src/components/LayerList.js
+++ src/components/LayerList.js
@@ -2,25 +2,10 @@ import React from 'react';
 
 import ViewLayer from './ViewLayer';
 
-export default class LayerList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ids = [];
-  }
-  componentDidMount() {
-    console.log(this.ids);
-  }
-  render() {
-    return this.props.viewLayerDataList.map(data => {
-      const {id} = data;
-      delete data.id;
-      return (
-        <ViewLayer
-          ref={ref => this.ids.push({id, ViewLayer: ref})}
-          key={id}
-          {...data}
-        />
-      );
-    });
-  }
+export default function LayerList({viewLayerDataList}) {
+  return viewLayerDataList.map(data => {
+    const {id} = data;
+    delete data.id;
+    return <ViewLayer key={id} {...data} />;
+  });
 }
