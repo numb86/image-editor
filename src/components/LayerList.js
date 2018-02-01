894ffa7da4397bea807bacd352c25e2325854b6b
numb86 <86numb@gmail.com>
2018-02-01 13:45:34 +0900

    必要なプロパティだけを ViewLayer に渡す。

--- src/components/LayerList.js
+++ src/components/LayerList.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import ViewLayer from './ViewLayer';
 
+const COMPONENT_USE_PROPS = ['isShow', 'imageData'];
+
 export default function LayerList({viewLayerDataList}) {
   return viewLayerDataList.map(data => {
     const {id} = data;
-    // TODO: 必要なプロパティだけをコンポーネントに渡す
-    return <ViewLayer key={id} {...data} />;
+    const props = COMPONENT_USE_PROPS.reduce((acc, prop) => {
+      acc[prop] = data[prop];
+      return acc;
+    }, {});
+    return <ViewLayer key={id} {...props} />;
   });
 }
