1c1e363c6e36276629e7fa15106619696fee2a9a
numb86 <86numb@gmail.com>
2018-02-03 11:13:29 +0900

    createImage を定義。

--- src/generateImageList.js
+++ src/generateImageList.js
@@ -1,4 +1,5 @@
 export const SPECIFY_PROPERTY = 'specifyProperty';
+export const CREATE_IMAGE = 'createImage';
 
 function specifyProperty(data, currentState, target) {
   let targetData;
@@ -15,10 +16,18 @@ function specifyProperty(data, currentState, target) {
   return updatedState;
 }
 
+function createImage(data, currentState) {
+  const updatedState = currentState.concat();
+  updatedState.unshift(data);
+  return updatedState;
+}
+
 export function generateImageList(type, data, currentState, target) {
   switch (type) {
     case SPECIFY_PROPERTY:
       return specifyProperty(data, currentState, target);
+    case CREATE_IMAGE:
+      return createImage(data, currentState);
     default:
       throw new Error('This type is not found.');
   }
