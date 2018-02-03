900fdababb1406c057ecf68d14869b5c14ca1679
numb86 <86numb@gmail.com>
2018-02-04 00:02:21 +0900

    add Flow LayerList.js ViewLayer.js

--- src/components/LayerList.js
+++ src/components/LayerList.js
@@ -1,16 +1,23 @@
+// @flow
 import React from 'react';
 
 import ViewLayer from './ViewLayer';
 
+import type {Image} from '../image';
+
 const COMPONENT_USE_PROPS = ['isShow', 'imageData'];
 
-export default function LayerList({viewLayerDataList}) {
+export default function LayerList({
+  viewLayerDataList,
+}: {
+  viewLayerDataList: Image[],
+}) {
   return viewLayerDataList.map(data => {
     const {id} = data;
     const props = COMPONENT_USE_PROPS.reduce((acc, prop) => {
       acc[prop] = data[prop];
       return acc;
     }, {});
-    return <ViewLayer key={id} {...props} />;
+    return <ViewLayer key={id} {...(props: any)} />;
   });
 }
--- src/components/ViewLayer.js
+++ src/components/ViewLayer.js
@@ -1,18 +1,28 @@
+// @flow
 import React from 'react';
 
-export default class ViewLayer extends React.Component {
-  constructor(props) {
+type Props = {
+  imageData: ImageData,
+  isShow: boolean,
+};
+
+export default class ViewLayer extends React.Component<Props> {
+  constructor(props: Props) {
     super(props);
     this.canvas = null;
     this.ctx = null;
   }
   componentDidMount() {
     const {imageData} = this.props;
+    if (!this.canvas) throw new Error('this.canvas is not found.');
     this.ctx = this.canvas.getContext('2d');
+    if (!this.canvas) throw new Error('this.canvas is not found.');
     this.canvas.width = imageData.width;
     this.canvas.height = imageData.height;
     this.ctx.putImageData(imageData, 0, 0);
   }
+  canvas: HTMLCanvasElement | null;
+  ctx: CanvasRenderingContext2D | null;
   render() {
     const {isShow} = this.props;
     return (
