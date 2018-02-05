f340bcc9dc93b63382193e614d68ad117ddbb11a
numb86 <86numb@gmail.com>
2018-02-05 20:15:27 +0900

    SPECIFY_PROPERTY 以外のテストを追加するための準備。

--- src/__tests__/generateImageList.test.js
+++ src/__tests__/generateImageList.test.js
@@ -1,6 +1,11 @@
 import assert from 'assert';
 
-import {generateImageList, SPECIFY_PROPERTY} from '../generateImageList';
+import {
+  generateImageList,
+  SPECIFY_PROPERTY,
+  ADD_IMAGE,
+  ADD_NEW_IMAGE,
+} from '../generateImageList';
 
 describe('generateImageList', () => {
   let originalImageList;
@@ -10,61 +15,74 @@ describe('generateImageList', () => {
       {id: 1, isShow: true, imageData: [1, 2, 3], obj: {a: 5}},
     ];
   });
-  it('指定したIDで変更対象のオブジェクトを指定できる', () => {
-    const imageData = 'newValue';
-    let newList = generateImageList({
-      type: SPECIFY_PROPERTY,
-      data: {imageData},
-      currentState: originalImageList,
-      target: 0,
-    });
-    assert(newList[0].imageData === imageData);
-    assert(newList[1].imageData !== imageData);
-    newList = generateImageList({
-      type: SPECIFY_PROPERTY,
-      data: {imageData},
-      currentState: originalImageList,
-      target: 1,
-    });
-    assert(newList[1].imageData === imageData);
-  });
-  it('リテラルである要素を副作用なく変更できる', () => {
-    const newList = generateImageList({
-      type: SPECIFY_PROPERTY,
-      data: {isShow: false},
-      currentState: originalImageList,
-      target: 0,
-    });
-    assert(newList[0].isShow === false);
-    assert(originalImageList[0].isShow === true);
-  });
-  it('オブジェクトである要素を副作用なく変更できる', () => {
-    const newList = generateImageList({
-      type: SPECIFY_PROPERTY,
-      data: {imageData: [7, 8, 9]},
-      currentState: originalImageList,
-      target: 0,
-    });
-    assert(newList[0].imageData[0] === 7);
-    assert(originalImageList[0].imageData[0] === 1);
-    assert(originalImageList[0].imageData !== newList[0].imageData);
-    // 変更する要素以外は参照渡しになるので注意
-    assert(originalImageList[0].obj === newList[0].obj);
-  });
-  it('複数の要素を副作用なく変更できる', () => {
-    const newList = generateImageList({
-      type: SPECIFY_PROPERTY,
-      data: {isShow: false, imageData: [7, 8, 9]},
-      currentState: originalImageList,
-      target: 0,
-    });
 
-    assert(newList[0].imageData[2] === 9);
-    assert(newList[1].imageData[2] === 3);
-    assert(originalImageList[0].imageData[2] === 3);
+  describe('SPECIFY_PROPERTY', () => {
+    it('指定したIDで変更対象のオブジェクトを指定できる', () => {
+      const imageData = 'newValue';
+      let newList = generateImageList({
+        type: SPECIFY_PROPERTY,
+        data: {imageData},
+        currentState: originalImageList,
+        target: 0,
+      });
+      assert(newList[0].imageData === imageData);
+      assert(newList[1].imageData !== imageData);
+      newList = generateImageList({
+        type: SPECIFY_PROPERTY,
+        data: {imageData},
+        currentState: originalImageList,
+        target: 1,
+      });
+      assert(newList[1].imageData === imageData);
+    });
+    it('リテラルである要素を副作用なく変更できる', () => {
+      const newList = generateImageList({
+        type: SPECIFY_PROPERTY,
+        data: {isShow: false},
+        currentState: originalImageList,
+        target: 0,
+      });
+      assert(newList[0].isShow === false);
+      assert(originalImageList[0].isShow === true);
+    });
+    it('オブジェクトである要素を副作用なく変更できる', () => {
+      const newList = generateImageList({
+        type: SPECIFY_PROPERTY,
+        data: {imageData: [7, 8, 9]},
+        currentState: originalImageList,
+        target: 0,
+      });
+      assert(newList[0].imageData[0] === 7);
+      assert(originalImageList[0].imageData[0] === 1);
+      assert(originalImageList[0].imageData !== newList[0].imageData);
+      // 変更する要素以外は参照渡しになるので注意
+      assert(originalImageList[0].obj === newList[0].obj);
+    });
+    it('複数の要素を副作用なく変更できる', () => {
+      const newList = generateImageList({
+        type: SPECIFY_PROPERTY,
+        data: {isShow: false, imageData: [7, 8, 9]},
+        currentState: originalImageList,
+        target: 0,
+      });
 
-    assert(newList[0].isShow === false);
-    assert(newList[1].isShow === true);
-    assert(originalImageList[0].isShow === true);
+      assert(newList[0].imageData[2] === 9);
+      assert(newList[1].imageData[2] === 3);
+      assert(originalImageList[0].imageData[2] === 3);
+
+      assert(newList[0].isShow === false);
+      assert(newList[1].isShow === true);
+      assert(originalImageList[0].isShow === true);
+    });
+  });
+
+  describe('ADD_IMAGE', () => {
+    it('指定したimageが先頭に追加される', () => {});
+    it('副作用がなくcurrentStateに影響を与えない', () => {});
+  });
+
+  describe('ADD_NEW_IMAGE', () => {
+    it('指定したサイズのimageが先頭に追加される', () => {});
+    it('副作用がなくcurrentStateに影響を与えない', () => {});
   });
 });
