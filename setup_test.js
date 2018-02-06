941f5185e651919b19861cf0dd17bcecffc565a2
numb86 <86numb@gmail.com>
2018-02-06 15:20:17 +0900

    テスト環境用のCanvasをクラス形式に書き換えた。

--- setup_test.js
+++ setup_test.js
@@ -5,24 +5,34 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
 
-const ctx = {
-  getImageData: (x, y, width, height) => ({
-    width,
-    height,
-    data: new Uint8ClampedArray(width * height * 4),
-  }),
-};
+/* eslint-disable class-methods-use-this */
+class Ctx {
+  constructor(canvas) {
+    this.canvas = canvas;
+  }
+  getImageData(x, y, width, height) {
+    return {
+      width,
+      height,
+      data: new Uint8ClampedArray(width * height * 4),
+    };
+  }
+}
+/* eslint-enable class-methods-use-this */
 
-const canvas = {
-  getContext: context => {
+class Canvas {
+  constructor() {
+    this.imageData = null;
+  }
+  getContext(context) {
     if (context !== '2d') throw new Error(`${context} is not support.`);
-    return ctx;
-  },
-};
+    return new Ctx(this);
+  }
+}
 
 global.document = {
   createElement: element => {
     if (element !== 'canvas') return null;
-    return canvas;
+    return new Canvas();
   },
 };
