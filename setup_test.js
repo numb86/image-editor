7bfe56d6b51ccd1778d785f535cab0e05ba095f1
numb86 <86numb@gmail.com>
2018-02-06 11:40:44 +0900

    テストの設定ファイルを書いて、 ADD_NEW_IMAGE のテストが通るようにした。

--- setup_test.js
+++ setup_test.js
@@ -4,3 +4,25 @@ import Adapter from 'enzyme-adapter-react-16';
 /* eslint-enable import/no-extraneous-dependencies */
 
 Enzyme.configure({adapter: new Adapter()});
+
+const ctx = {
+  getImageData: (x, y, width, height) => ({
+    width,
+    height,
+    data: new Uint8ClampedArray(width * height * 4),
+  }),
+};
+
+const canvas = {
+  getContext: context => {
+    if (context !== '2d') throw new Error(`${context} is not support.`);
+    return ctx;
+  },
+};
+
+global.document = {
+  createElement: element => {
+    if (element !== 'canvas') return null;
+    return canvas;
+  },
+};
