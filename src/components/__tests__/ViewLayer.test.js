6448506efd65321141c2f891b6500a257c01b729
numb86 <86numb@gmail.com>
2018-02-06 15:52:50 +0900

    putImageData が関係するテストも実装した。

--- src/components/__tests__/ViewLayer.test.js
+++ src/components/__tests__/ViewLayer.test.js
@@ -5,6 +5,8 @@ import assert from 'assert';
 import ViewLayer from '../ViewLayer';
 
 describe('ViewLayer', () => {
+  const WIDTH = 100;
+  const HEIGHT = 90;
   function createImageData(width, height, data) {
     return {
       width,
@@ -12,12 +14,23 @@ describe('ViewLayer', () => {
       data: Uint8ClampedArray.from(data),
     };
   }
-  const imageData = createImageData(100, 100, [0, 1, 2, 3, 4]);
+  const imageData = createImageData(WIDTH, HEIGHT, [0, 1, 2, 3, 4]);
   const wrapper = shallow(<ViewLayer key="0" imageData={imageData} isShow />, {
     disableLifecycleMethods: true,
   });
-  it.skip('imageDataで渡されたサイズのCanvasが描画される', () => {});
-  it.skip('渡されたimageDataの内容が描画される', () => {});
+  it('渡されたimageDataの内容が描画される', () => {
+    const inst = wrapper.instance();
+    inst.canvas = document.createElement('canvas');
+    inst.componentDidMount();
+    assert(inst.canvas.imageData.data === imageData.data);
+  });
+  it('imageDataで渡されたサイズのCanvasが描画される', () => {
+    const inst = wrapper.instance();
+    inst.canvas = document.createElement('canvas');
+    inst.componentDidMount();
+    assert(inst.canvas.imageData.width === WIDTH);
+    assert(inst.canvas.imageData.height === HEIGHT);
+  });
   it('props.isShowに応じて、表示非表示が切り替わる', () => {
     let {display} = wrapper.prop('style');
     assert(display === 'block');
