14695232e462c68a1542e2366342841d4f83baac
numb86 <86numb@gmail.com>
2018-02-10 20:00:57 +0900

    オブジェクト版のスプレッド演算子は、現在のBabelの設定では使えなかった。

--- src/components/actionLayer/ActionLayer.js
+++ src/components/actionLayer/ActionLayer.js
@@ -3,10 +3,9 @@ import React from 'react';
 import DrawLineLayer from './DrawLineLayer';
 
 export default function ActionLayer(props) {
-  const {activeActionLayer, ...other} = props;
-  switch (activeActionLayer) {
+  switch (props.activeActionLayer) {
     case 'drawLine':
-      return <DrawLineLayer {...other} />;
+      return <DrawLineLayer {...props} />;
     default:
       throw new Error('Could not find ActionLayer.');
   }
