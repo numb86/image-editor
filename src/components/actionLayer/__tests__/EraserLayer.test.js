d348615a92431fb2c233fa380c6b28176472b1a4
numb86 <86numb@gmail.com>
2018-02-14 13:12:55 +0900

    EraserLayer のテストを作成。

---
+++ src/components/actionLayer/__tests__/EraserLayer.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import assert from 'assert';
+
+import EraserLayer from '../EraserLayer';
+
+describe('EraserLayer', () => {
+  const IMAGE_DATA = {foo: 'boo'};
+  const wrapper = shallow(
+    <EraserLayer imageData={IMAGE_DATA} updateImageData={() => {}} />
+  );
+  it('MouseMoveActionLayer を返す', () => {
+    assert(wrapper.length === 1);
+    assert(wrapper.find('MouseMoveActionLayer').length === 1);
+  });
+  it('props.imageData がそのままバケツリレーされる', () => {
+    assert(
+      wrapper.find('MouseMoveActionLayer').prop('imageData') === IMAGE_DATA
+    );
+  });
+  it('callbackDidMount, callbackDidUpdate, executeAction, imageData を EraserLayer に渡す', () => {
+    const expectedProps = [
+      'callbackDidMount',
+      'callbackDidUpdate',
+      'executeAction',
+      'imageData',
+    ];
+    const wrapperProps = Object.keys(
+      wrapper.find('MouseMoveActionLayer').props()
+    );
+    assert(
+      wrapperProps.every(prop =>
+        expectedProps.some(expectedProp => prop === expectedProp)
+      )
+    );
+  });
+});
