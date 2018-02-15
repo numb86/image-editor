064e03a6a2b642fee3141d29f416887258afe10d
numb86 <86numb@gmail.com>
2018-02-15 14:01:21 +0900

    add Flow generateActionLayerSettings.js

--- src/state/generateActionLayerSettings.js
+++ src/state/generateActionLayerSettings.js
@@ -1,8 +1,32 @@
+// @flow
+import type {ActionLayerName} from '../components/actionLayer/ActionLayer';
+
 export const SPECIFY_CONTEXT_PROPERTY: 'specifyContextProperty' =
   'specifyContextProperty';
+
 const CTX: 'ctx' = 'ctx';
 
-function specifyProperty(currentState, target, key, data) {
+type ContextSetting = {
+  lineWidth?: number,
+  strokeStyle?: string,
+  lineCap: string,
+  lienJoin: string,
+};
+
+type MouseMoveActionLayerSetting = {
+  ctx: ContextSetting,
+};
+
+type ActionLayerSettings = {
+  [ActionLayerName]: MouseMoveActionLayerSetting,
+};
+
+function specifyProperty(
+  currentState: ActionLayerSettings,
+  target: ActionLayerName,
+  key: typeof CTX,
+  data: ContextSetting
+): ActionLayerSettings {
   const newKeyValue = Object.assign({}, currentState[target][key], data);
   const newTargetValue = Object.assign({}, currentState[target], {
     [key]: newKeyValue,
@@ -15,7 +39,12 @@ export function generateActionLayerSettings({
   currentState,
   data,
   target,
-}) {
+}: {
+  type: typeof SPECIFY_CONTEXT_PROPERTY,
+  currentState: ActionLayerSettings,
+  data: ContextSetting,
+  target: ActionLayerName,
+}): ActionLayerSettings {
   switch (type) {
     case SPECIFY_CONTEXT_PROPERTY:
       return specifyProperty(currentState, target, CTX, data);
