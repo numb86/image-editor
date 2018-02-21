9481bbcce18541e1c8685601b95f41d69b663580
numb86 <86numb@gmail.com>
2018-02-21 20:50:24 +0900

    入出力共に、 ImageData にした。

--- src/changeImageDataPixel.js
+++ src/changeImageDataPixel.js
@@ -1,9 +1,14 @@
 // @flow
 
-export default function invertNegaPosi(
-  src: Uint8ClampedArray
-): Uint8ClampedArray {
-  return src.map(
-    (pixel, index) => ((index + 1) % 4 === 0 ? pixel : 255 - pixel)
-  );
+export default function invertNegaPosi(src: ImageData): ImageData {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = src.width;
+  canvas.height = src.height;
+  ctx.putImageData(src, 0, 0);
+  const dest = ctx.getImageData(0, 0, src.width, src.height);
+  dest.data.forEach((pixel, index) => {
+    dest.data[index] = (index + 1) % 4 === 0 ? pixel : 255 - pixel;
+  });
+  return dest;
 }
