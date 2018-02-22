5e694908f8803916b225b8b0e211a83276c5fe26
numb86 <86numb@gmail.com>
2018-02-22 16:10:40 +0900

    applyGrayScale のテストを作成。

--- src/__tests__/editImageDataPixel.test.js
+++ src/__tests__/editImageDataPixel.test.js
@@ -1,19 +1,19 @@
 import assert from 'assert';
 
-import invertNegaPosi from '../editImageDataPixel';
+import {invertNegaPosi, applyGrayScale} from '../editImageDataPixel';
 
 describe('editImageDataPixel', () => {
+  let canvas;
+  let ctx;
+  let srcImageData;
+  beforeEach(() => {
+    canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = 3;
+    canvas.height = 1;
+    srcImageData = ctx.createImageData(3, 1);
+  });
   describe('invertNegaPosi', () => {
-    let canvas;
-    let ctx;
-    let srcImageData;
-    beforeEach(() => {
-      canvas = document.createElement('canvas');
-      ctx = canvas.getContext('2d');
-      canvas.width = 3;
-      canvas.height = 1;
-      srcImageData = ctx.createImageData(3, 1);
-    });
     it('rgbを反転させる。aは反転させず元の値のまま', () => {
       const src = [
         255, // r
@@ -52,6 +52,37 @@ describe('editImageDataPixel', () => {
       assert(dest[1] === 145);
       assert(srcImageData.data[0] === 100);
       assert(srcImageData.data[1] === 110);
+    });
+  });
+  describe('applyGrayScale', () => {
+    it('各ピクセルのRGBを全て、RGBの中間値に変換する。aは反転させず元の値のまま', () => {
+      const src = [
+        100, // r
+        100, // g
+        100, // b
+        1, // a
+        30, // r
+        200, // g
+        100, // b
+        0, // a
+        80, // r
+        200, // g
+        54, // b
+        1, // a
+      ];
+      srcImageData.data.forEach((pixel, index) => {
+        srcImageData.data[index] = src[index];
+      });
+      const dest = applyGrayScale(srcImageData).data;
+      assert(
+        dest[0] === 100 && dest[1] === 100 && dest[2] === 100 && dest[3] === 1
+      );
+      assert(
+        dest[4] === 110 && dest[5] === 110 && dest[6] === 110 && dest[7] === 0
+      );
+      assert(
+        dest[8] === 111 && dest[9] === 111 && dest[10] === 111 && dest[11] === 1
+      );
     });
   });
 });
