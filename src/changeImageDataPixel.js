0bb0b669bd975630e48c111fa33441eeef5915ed
numb86 <86numb@gmail.com>
2018-02-22 15:26:36 +0900

    再利用性が高い形に書き換え。

--- src/changeImageDataPixel.js
+++ src/changeImageDataPixel.js
@@ -1,17 +1,26 @@
 // @flow
 
-export default function invertNegaPosi(src: ImageData): ImageData {
+function generateImageData(
+  src: ImageData,
+  callback: ImageData => ImageData
+): ImageData {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   canvas.width = src.width;
   canvas.height = src.height;
   ctx.putImageData(src, 0, 0);
   const dest = ctx.getImageData(0, 0, src.width, src.height);
+  return callback(dest);
+}
+
+export default function invertNegaPosi(src: ImageData): ImageData {
   // テスト環境（jsdomを使用）では、getImageDataでrgbをコピーできない（必ず0になる）ので、
   // src.data[index]の値を使っている
-  dest.data.forEach((pixel, index) => {
-    dest.data[index] =
-      (index + 1) % 4 === 0 ? src.data[index] : 255 - src.data[index];
+  return generateImageData(src, dest => {
+    dest.data.forEach((pixel, index) => {
+      dest.data[index] =
+        (index + 1) % 4 === 0 ? src.data[index] : 255 - src.data[index];
+    });
+    return dest;
   });
-  return dest;
 }
