14360d80fe39d5d6a65c5726369aef3c4216b8f5
numb86 <86numb@gmail.com>
2018-02-22 14:56:01 +0900

    src.data[index] の値を使う。

--- src/changeImageDataPixel.js
+++ src/changeImageDataPixel.js
@@ -7,8 +7,11 @@ export default function invertNegaPosi(src: ImageData): ImageData {
   canvas.height = src.height;
   ctx.putImageData(src, 0, 0);
   const dest = ctx.getImageData(0, 0, src.width, src.height);
+  // テスト環境（jsdomを使用）では、getImageDataでrgbをコピーできない（必ず0になる）ので、
+  // src.data[index]の値を使っている
   dest.data.forEach((pixel, index) => {
-    dest.data[index] = (index + 1) % 4 === 0 ? pixel : 255 - pixel;
+    dest.data[index] =
+      (index + 1) % 4 === 0 ? src.data[index] : 255 - src.data[index];
   });
   return dest;
 }
