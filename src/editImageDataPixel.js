7bae3c4d3930cbfd56eaaebf688e189751e56111
numb86 <86numb@gmail.com>
2018-02-23 11:45:45 +0900

    applyGrayScale を定義。

--- src/editImageDataPixel.js
+++ src/editImageDataPixel.js
@@ -13,14 +13,29 @@ function generateImageData(
   return callback(dest);
 }
 
-export default function invertNegaPosi(src: ImageData): ImageData {
+export function invertNegaPosi(src: ImageData): ImageData {
   // テスト環境（jsdomを使用）では、getImageDataでrgbをコピーできない（必ず0になる）ので、
   // src.data[index]の値を使っている
+  // 他の関数も同様
   return generateImageData(src, dest => {
     dest.data.forEach((pixel, index) => {
       dest.data[index] =
         (index + 1) % 4 === 0 ? src.data[index] : 255 - src.data[index];
     });
+    return dest;
+  });
+}
+
+export function applyGrayScale(src: ImageData): ImageData {
+  return generateImageData(src, dest => {
+    for (let i = 0; i < dest.data.length; i += 4) {
+      const {data} = src;
+      const avg = Math.floor((data[i] + data[i + 1] + data[i + 2]) / 3);
+      dest.data[i] = avg;
+      dest.data[i + 1] = avg;
+      dest.data[i + 2] = avg;
+      dest.data[i + 3] = data[i + 3];
+    }
     return dest;
   });
 }
