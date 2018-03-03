7b42c14a32d7fa26c6fec24074d3836ca9635ce8
numb86 <86numb@gmail.com>
2018-03-03 23:00:06 +0900

    SASS のビルドを webpack でやるようにした。

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
     'public/bundle': './index.js',
+    'public/css/index': './style.js',
     'experiment/deterioration/deterioration': './experiment/deterioration.js',
   },
   output: {
@@ -25,8 +27,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {loader: 'css-loader', options: {minimize: true, url: false}},
+            'sass-loader',
+          ],
+        }),
+      },
     ],
   },
+  plugins: [new ExtractTextPlugin({filename: '[name].css'})],
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
     historyApiFallback: true,
