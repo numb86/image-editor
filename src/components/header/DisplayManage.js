4ae943a5257e5345d212e1c7f00b1712444bc3e1
numb86 <86numb@gmail.com>
2018-03-14 18:11:20 +0900

    エラーのときは文字の色を変えるようにした。

--- src/components/header/DisplayManage.js
+++ src/components/header/DisplayManage.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import ClassNames from 'classnames';
 
 import type {DisplayType} from '../Display';
 
@@ -12,6 +13,7 @@ type Props = {
 type State = {
   widthString: string,
   heightString: string,
+  error: boolean,
 };
 
 export default class DisplayManage extends React.Component<Props, State> {
@@ -21,6 +23,7 @@ export default class DisplayManage extends React.Component<Props, State> {
     this.state = {
       widthString: String(width),
       heightString: String(height),
+      error: false,
     };
   }
   render() {
@@ -40,28 +43,41 @@ export default class DisplayManage extends React.Component<Props, State> {
         </button>
         <input
           type="number"
+          className={ClassNames({'error-message': this.state.error})}
           value={this.state.widthString}
           onChange={e => {
-            this.setState({widthString: e.currentTarget.value});
+            this.setState({
+              widthString: e.currentTarget.value,
+              error: false,
+            });
           }}
         />px
         <input
           type="number"
+          className={ClassNames({'error-message': this.state.error})}
           value={this.state.heightString}
           onChange={e => {
-            this.setState({heightString: e.currentTarget.value});
+            this.setState({
+              heightString: e.currentTarget.value,
+              error: false,
+            });
           }}
         />px
         <button
           data-test="specify-display-size"
           onClick={() => {
+            let error = false;
             const {widthString, heightString} = this.state;
-            if (!widthString || !heightString) return;
+            if (!widthString || !heightString) error = true;
             const width = Number(widthString);
             const height = Number(heightString) - 0;
-            if (isNaN(width) || isNaN(height)) return;
-            if (width <= 0 || height <= 0) return;
-            if (width >= 10000 || height >= 10000) return;
+            if (isNaN(width) || isNaN(height)) error = true;
+            if (width <= 0 || height <= 0) error = true;
+            if (width >= 10000 || height >= 10000) error = true;
+            if (error) {
+              this.setState({error: true});
+              return;
+            }
             updateDisplaySize(width, height);
           }}
         >
