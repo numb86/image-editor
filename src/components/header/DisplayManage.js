94fefc30c098965dce37398370374ca097fe35ba
numb86 <86numb@gmail.com>
2018-03-14 16:32:40 +0900

    DisplayManage 完成。

--- src/components/header/DisplayManage.js
+++ src/components/header/DisplayManage.js
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 
-// TODO: use Fragments
 export default function DisplayManage({
   updateDisplaySize,
   showImageDatas,
@@ -9,12 +8,45 @@ export default function DisplayManage({
   updateDisplaySize: (width: number, height: number) => void,
   showImageDatas: ImageData[],
 }) {
+  let width = null;
+  let height = null;
   return (
     <div className="display-manage">
-      <button>キャンバスをレイヤーの最大値に合わせる</button>
-      <input type="number" />px
-      <input type="number" />px
-      <button>リサイズ</button>
+      <button
+        data-test="fit-max-view-layer"
+        onClick={() => {
+          if (showImageDatas.length === 0) return;
+          const maxWidth = Math.max(...showImageDatas.map(i => i.width));
+          const maxHeight = Math.max(...showImageDatas.map(i => i.height));
+          updateDisplaySize(maxWidth, maxHeight);
+        }}
+      >
+        キャンバスをレイヤーの最大値に合わせる
+      </button>
+      <input
+        type="number"
+        onChange={e => {
+          width = e.currentTarget.value;
+        }}
+      />px
+      <input
+        type="number"
+        onChange={e => {
+          height = e.currentTarget.value;
+        }}
+      />px
+      <button
+        data-test="specify-display-size"
+        onClick={() => {
+          if (!width || !height) return;
+          if (!Number.isFinite(width) || !Number.isFinite(height)) return;
+          if (width <= 0 || height <= 0) return;
+          if (width >= 10000 || height >= 10000) return;
+          updateDisplaySize(width, height);
+        }}
+      >
+        リサイズ
+      </button>
     </div>
   );
 }
