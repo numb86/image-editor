d639430a174d653d5450a3a92a4e9898dbe3814d
numb86 <86numb@gmail.com>
2018-03-22 11:15:45 +0900

    環境変数によってビルドする内容を変える。

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,53 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const LicenseInfoWebpackPlugin = require('license-info-webpack-plugin').default;
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = env => ({
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    bundle: './index.js',
-    'css/index': './style.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        use: 'babel-loader',
-      },
-      {
-        test: /\.scss/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {loader: 'css-loader', options: {minimize: true, url: false}},
-            'sass-loader',
-          ],
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new ExtractTextPlugin({filename: '[name].css'}),
-    new LicenseInfoWebpackPlugin({
-      glob: '{LICENSE,license,License}*',
-    }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        output: {
-          comments: /^\**!|@preserve|@license|@cc_on/,
+module.exports = env => {
+  const plugins = [new ExtractTextPlugin({filename: '[name].css'})];
+  if (env === 'production') {
+    plugins.push(
+      new LicenseInfoWebpackPlugin({
+        glob: '{LICENSE,license,License}*',
+      }),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: /^\**!|@preserve|@license|@cc_on/,
+          },
         },
-      },
-    }),
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-  },
-});
+      })
+    );
+  }
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      bundle: './index.js',
+      'css/index': './style.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          use: 'babel-loader',
+        },
+        {
+          test: /\.scss/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {loader: 'css-loader', options: {minimize: true, url: false}},
+              'sass-loader',
+            ],
+          }),
+        },
+      ],
+    },
+    plugins,
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+    },
+  };
+};
