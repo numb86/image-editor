5e15d0c7088df3f05f9a88b7c771e267ecc9db12
numb86 <86numb@gmail.com>
2018-04-07 17:38:27 +0900

    ActiveActionLayerState を定義。

---
+++ src/components/header/ActiveActionLayerState.js
@@ -0,0 +1,48 @@
+// @flow
+import React from 'react';
+
+import {DRAW_LINE, ERASER} from '../actionLayer/ActionLayer';
+
+import type {ActionLayerName} from '../actionLayer/ActionLayer';
+import type {MouseMoveActionLayerSetting} from '../../state/generateActionLayerSettings';
+
+const DRAW_LINE_ICON_CLASS_NAME = 'fa-pencil';
+const ERASER_ICON_CLASS_NAME = 'fa-eraser';
+
+const DATA_URL_TRANSPARENT =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAP0lEQVQ4T2OcOXPmfwYiwL1794hQxcDAOGogznAaDUOcQTMEkk15eTlROUVJSYm4nDJqIM5wGg1D3Dll0CcbAMflUeHimVpmAAAAAElFTkSuQmCC';
+
+export default function ActiveActionLayerState({
+  activeActionLayer,
+  setting,
+}: {
+  activeActionLayer: ActionLayerName,
+  setting: MouseMoveActionLayerSetting,
+}) {
+  const getIconClassName = () => {
+    switch (activeActionLayer) {
+      case DRAW_LINE:
+        return DRAW_LINE_ICON_CLASS_NAME;
+      case ERASER:
+        return ERASER_ICON_CLASS_NAME;
+      default:
+        throw new Error('This actionLayer is not defined.');
+    }
+  };
+  const getColorStateStyle = () => {
+    if (activeActionLayer === ERASER) {
+      return {backgroundImage: `url(${DATA_URL_TRANSPARENT})`};
+    }
+    return {backgroundColor: setting.ctx.strokeStyle};
+  };
+  return (
+    <span>
+      <span className={`active-action-layer-icon fa ${getIconClassName()}`} />
+      <span
+        data-test="active-action-layer-color"
+        className="active-action-layer-color"
+        style={getColorStateStyle()}
+      />
+    </span>
+  );
+}
